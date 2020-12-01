663fe70e513996b30f1914ed73148a35ce26636f
Wsad <nodetestwsad@gmail.com>
2020-12-01 20:41:46 +0100

    p

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-console.log('Piekosowy seeen!')
+console.log('Piekosowy xd seeen!')
 
 app.use('/user', userRoutes)
 
