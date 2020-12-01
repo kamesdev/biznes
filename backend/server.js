2a4998457b5cd195188a4c80f66491af92599056
kamesdev <kamilpienkos1@gmail.com>
2020-12-01 20:14:04 +0100

    someee

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,6 @@ app.use('/user', userRoutes)
 const PORT = process.env.PORT || 5000
 const NODE_ENV = process.env.NODE_ENV || 'developmnet'
 
-console.log('something')
 
 app.listen(PORT, () => {
   console.log(`Listening in ${NODE_ENV} on port ${PORT}`)
