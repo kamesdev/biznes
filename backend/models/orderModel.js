8fee5dee6f26aa3fd3a8ef5e8937b294ca770b9e
kamesdev <kamilpienkos1@gmail.com>
2020-12-02 19:43:20 +0100

    commit

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,28 +1,56 @@
 import mongoose from 'mongoose'
 
-const OrderSchema = mongoose.Schema({
-  service: {
-    type: Schema.Types.ObjectId,
-    ref: 'Service'
-  },
-  
-  quantity: {
-  
+const orderSchema = mongoose.Schema({
+  user: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
   },
 
-  isPaid: {
-    default: false
-  },
-
-  paidAt: {
-    type: Date,
-  },
-
-  link: {
+  orderedServices: [
+    {
+      name: { type: String, required: true },
+      qty: { type: Number, required: true },
+      image: { type: String, required: true },
+      price: { type: Number, required: true },
+      product: {
+        required: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Service',
+      },
+    }
+  ],
+  details: {
     type: String,
     required: true,
-  }
-
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  isPaid: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  paidAt: {
+    type: Date.now(),
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+    default: 0.0,
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  paymentResult: {
+    id: { type: String },
+    status: { type: String },
+    update_time: { type: String },
+    email_address: { type: String },
+  },
 })
 
-export default mongoose.model('User', OrderSchema)
+export default mongoose.model('Order', orderSchema)
